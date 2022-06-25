7282ce59ab8ae9db6af7e417349dd248419b2cb4
Truman Gao <mr.trumgao@gmail.com>
2022-06-25 11:06:13 -0700

    set term block containers and initialize term blocks on start

--- course-planner.js
+++ course-planner.js
@@ -2,10 +2,12 @@ class Course {
   constructor(name, termSessionOffering, prereqs, courseTakenOn, takeCourse){
     this.name = name;
   }
+
+  testMethod1(test){
+    console.log('running test method', test);
+  }
 }
 
-const course = new Course('myName');
-console.log(course.name);
 
 
 // INITIALIZE //
@@ -15,7 +17,7 @@ let addedCourses = [];
 
 async function init(){
   // create term blocks
-  cr_termBlock(20);
+  cr_termBlock(17);
 
   // fill courses
   fillCourses();
@@ -43,6 +45,11 @@ function fillCourses(){
 }
 
 
+
+
+
+// Generate term blocks and set data on init //
+
 function cr_termBlock(num_termBlocks = null){
   if (num_termBlocks === null) num_termBlocks = 1;
   
@@ -50,7 +57,12 @@ function cr_termBlock(num_termBlocks = null){
   
   for (let i = 0; i < num_termBlocks; i++){ 
     const termBlockClone = termBlock.cloneNode(true);
-    document.body.appendChild(termBlockClone);
+    setTermBlockData(termBlockClone);
+
+    // add term block to it's year container
+    const termYear = termBlockClone.getAttribute('data-year');
+    const container = document.querySelector(`.year-container[data-year='${termYear}']`)
+    container.appendChild(termBlockClone);
   }
 }
 
@@ -60,7 +72,8 @@ function setTermBlockData(termBlock){
   
   const termSessions = ['summer', 'fall', 'spring'];
   const termSession = termSessions[termNumber % 3]; // mod returns 0, 1, 2, (term 1 returns 1, term 3 returns 0, so summer term is 0th index)
-  termBlock.setAttribute('data-term-session', toString(termSession));
+  termBlock.setAttribute('data-term-session', termSession);
+  termBlock.setAttribute('data-year', parseInt(termNumber/3) + 1); // truncate num plus 1 returns term year given amount of terms
 
   termBlock.setAttribute('data-term-type', 'study');
   termBlock.setAttribute('data-max-course-num', '4');
