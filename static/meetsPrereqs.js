a60133540790326de6a25924b52e020cfa71a167
Truman Gao <mr.trumgao@gmail.com>
2022-07-16 22:12:22 -0700

    working version

--- static/meetsPrereqs.js
+++ static/meetsPrereqs.js
@@ -1,58 +1,59 @@
-export default function meetsPrereqs(scheduleBlock, completedCourses, curTermCourses, obj = null){
-  const prereqs = scheduleBlock.courseData.requirements;
-  if (obj = null) obj = prereqs;
+/**
+ * ASSUMPTIONS 
+ *  - requirement objects always contain only one key
+ *  - every object has array entrie and array entry has object vice versa
+ * 
+ * @param {Element} scheduleBlock 
+ * @param {Array} completedCourses 
+ * @param {Array} curTermCourses 
+ * @param {Object} requirement 
+ * @returns 
+ */
+export default function meetsPrereqs(scheduleBlock, completedCourses, curTermCourses, requirement = null){
+  if (scheduleBlock.courseData.requirements.length === 0) return true;
+  const prereqs = scheduleBlock.courseData.requirements[0];
+  if (requirement === null) requirement = prereqs;
 
-  let booleanList = []
+  const requirementType = Object.keys(requirement)[0] // assume one key for every object
 
-  for (let key in obj){
-    if ( key === "Complete 1 of the following" || key === "Complete 1 of: " ){
-      if (typeof obj[key] === "object"){
-      // check if one of the values are met
-        let metReqs = meetsPrereqs(scheduleBlock, obj);
-        if ( metReqs.includes(true) ) return [true];
+  // console.log(requirement)
+  let meets = [];
+
+  requirement[requirementType].forEach(item => {
+    // console.log(item)
+    // if its another object, run recurse with type
+    if ( typeof item === "object"){
+      meets.push( meetsPrereqs(scheduleBlock, completedCourses, curTermCourses, item) )
+    }
+    // is string
+    else { 
+      // if can take concurrently
+      if ( requirementType.split(" ").includes("concurrently") ){
+        meets.push( [...completedCourses, ...curTermCourses].includes(item) );
       }
-      else {
-        let metReqs = [];
-        obj[key].forEach(course => {
-          if ( completedCourses.includes( course ) ) metReqs.push(true);
-          else metReqs.push(false);
-        });
-        return [metReqs.includes(true)];
+      else{
+        meets.push( completedCourses.includes(item) );
       }
     }
-    else if ( key === "Complete all of the following" || key === "Complete all of: " ){
-      if (typeof obj[key] === "object"){
-        // check if one of the values are met
-        let metReqs = meetsPrereqs(scheduleBlock, obj);
-        if ( metReqs.includes(false) ) return [false];
-      }
-      else {
-        let metReqs = [];
-        obj[key].forEach(course => {
-          if ( completedCourses.includes( course ) ) metReqs.push(true);
-          else metReqs.push(false)
-        });
-        return [!metReqs.includes(false)];
-      }
-    }
-    else if ( key === "Completed or concurrently enrolled in 1 of: " ){
-      if (typeof obj[key] === "object"){
-        // check if one of the values are met
-          let metReqs = meetsPrereqs(scheduleBlock, obj);
-          if ( metReqs.includes(true) ) return [true];
-        }
-        else {
-          let metReqs = [];
-          obj[key].forEach(course => {
-            if ( completedCourses.includes( course ) || curTermCourses.includes( course ) ) metReqs.push(true);
-            else metReqs.push(false);
-          });
-          return [metReqs.includes(true)];
-        }
-      }
-    else { console.warn("key not checked for pre req: ", key)};
+
+  });
+
+  // console.log(requirementType)
+  // console.log(meets)
+  if ( 
+    requirementType === "Complete all of the following" || 
+    requirementType === "Complete all of: " ||
+    requirementType === "Completed or concurrently enrolled in all of: "
+    // requirementType === "Complete all of the following" || //note: yet to find mentioned pattern
+    ){
+    return meets.includes(true) && !meets.includes(false); 
+  }
+  if ( 
+    requirementType === "Complete  1  of the following" || 
+    requirementType === "Complete 1 of: " ||
+    requirementType === "Completed or concurrently enrolled in 1 of: "
+    ){
+    return meets.includes(true); 
   }
 
-
-  return ( !booleanList.includes(false) );
 }
