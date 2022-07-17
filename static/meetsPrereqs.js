5ae2a25545e739990d8e7787d5a8788c82d540e3
Truman Gao <mr.trumgao@gmail.com>
2022-07-17 11:55:22 -0700

    implement add assume completed

--- static/meetsPrereqs.js
+++ static/meetsPrereqs.js
@@ -2,6 +2,8 @@
  * ASSUMPTIONS 
  *  - requirement objects always contain only one key
  *  - every object has array entrie and array entry has object vice versa
+ *  - course requirements contain master bullet (1 bullet that contains all other bullets)
+ *    - TODO - stated above is not true, and needs to be account for (ex. MATH101)
  * 
  * @param {Element} scheduleBlock 
  * @param {Array} completedCourses 
@@ -9,7 +11,9 @@
  * @param {Object} requirement 
  * @returns 
  */
-export default function meetsPrereqs(scheduleBlock, completedCourses, curTermCourses, requirement = null){
+export function meetsPrereqs(scheduleBlock, completedCourses, curTermCourses, requirement = null){
+  // if (requirement === null) addAssumeCompleted(completedCourses);
+
   if (scheduleBlock.courseData.requirements.length === 0) return true;
   const prereqs = scheduleBlock.courseData.requirements[0];
   if (requirement === null) requirement = prereqs;
@@ -43,17 +47,31 @@ export default function meetsPrereqs(scheduleBlock, completedCourses, curTermCou
   if ( 
     requirementType === "Complete all of the following" || 
     requirementType === "Complete all of: " ||
-    requirementType === "Completed or concurrently enrolled in all of: "
+    requirementType === "Completed or concurrently enrolled in all of: " ||
+    requirementType === "Earn a minimum grade of C+ in each of the following: "
     // requirementType === "Complete all of the following" || //note: yet to find mentioned pattern
     ){
     return meets.includes(true) && !meets.includes(false); 
   }
-  if ( 
+  else if ( 
     requirementType === "Complete  1  of the following" || 
     requirementType === "Complete 1 of: " ||
     requirementType === "Completed or concurrently enrolled in 1 of: "
     ){
     return meets.includes(true); 
   }
+  else {
+    console.warn("not accounting for the following requirement type: ", requirementType, meets, requirement);
+  }
 
+}
+
+export function addAssumeCompleted(completed){
+  const assumeCompleted = [
+    'Foundations of Math 12',
+    'Mathematics 12',
+    'Pre-Calculus 12',
+    'Completed Pre-Calculus 12 or Principles of Mathematics 12  with a minimum grade of B (73%) ',
+  ];
+  completed.push(...assumeCompleted);
 }
